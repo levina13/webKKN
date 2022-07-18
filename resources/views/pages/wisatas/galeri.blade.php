f8399d622d78e952ed31f44c9d71f0b951539d6b
erlianafajar <erlinafjr@gmail.com>
2022-07-18 21:24:31 +0700

    kuliner

--- resources/views/pages/wisatas/galeri.blade.php
+++ resources/views/pages/wisatas/galeri.blade.php
@@ -8,13 +8,93 @@
 	</header>
 <main>
 
-    <section class="section-popular main-section" id="popular" style="margin-bottom: -300px !important;">
+    <section class="section-popular main-section" id="popular" style="margin-bottom: -10px !important;">
         <div class="container">
             <section class="section-popular-content" id="popularContent">
                 <div class="container">
                     <div class="card border-0 shadow">
                         <div class="card-body">
-                            <div class="section-popular-travel row justify-content-center">
+                            {{--<div class="section-popular-travel row justify-content-center">--}}
+                                <h3 class="title">
+                                    <span><b># Galeri Wisata Sumber Banteng</b></span>
+                                </h3>
+                                <div class="row pt-1 gx-1 gy-1">
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row pt-4 gx-4 gy-4">
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row pt-4 gx-4 gy-4">
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://cdn.antaranews.com/cache/730x487/2019/07/21/Wisata-Sumber-Banteng-Kediri-210719-pf-6.jpg" alt="...." />
+                                        </div>
+                                    </div>
+                                </div>
                                 {{-- @foreach ($wisatas as $wisata)
                                 <div class="col-sm-6 col-md-4 col-lg-3">
                                     <div class="border-0 shadow rounded card-travel text-center d-flex flex-column" style="background-image: url('{{ urlencode($wisata->gambar) }}');" >
--- resources/views/pages/wisatas/kuliner.blade.php
+++ resources/views/pages/wisatas/kuliner.blade.php
@@ -2,6 +2,8 @@
 @section('layout_title', 'Kuliner Terpopuler')
 @section('page_title', "Kuliner")
 @section('layout_content')
+<link rel="stylesheet" href="/layoutkuliner.css" />
+
 		<p class="mt-3">
 			Beberapa kuliner khas <br>
             di sekitar Sumber Banteng
@@ -12,13 +14,71 @@
 	</header>
 <main>
 
-    <section class="section-popular main-section" id="popular" style="margin-bottom: -300px !important;">
+    <section class="section-popular main-section" id="popular" style="margin-bottom: -100px !important;">
         <div class="container">
             <section class="section-popular-content" id="popularContent">
                 <div class="container">
                     <div class="card border-0 shadow">
                         <div class="card-body">
-                            <div class="section-popular-travel row justify-content-center">
+                            {{--<div class="section-popular-travel row justify-content-center">--}}
+
+                                <div class="row pt-4 gx-4 gy-4">
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://www.itrip.id/wp-content/uploads/2019/12/Tempat-Makan-Kediri.jpg" alt="...." />
+                                            <div class="card-body">
+                                              <h5 class="card-title">Bakso - Bu Lastri</h5>
+                                              <p class="card-text">Bakso Jumbo, Bakso Bakar, Aneka Camilan</p>
+                                                <div class="number">
+                                                    <div class="medium infoitem"></div>
+                                                    "4.6"                     1,2 km
+                                            </div>
+                                          </div>
+                                    </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://www.itrip.id/wp-content/uploads/2019/12/Tempat-Makan-Kediri.jpg" alt="...." />
+                                            <div class="card-body">
+                                              <h5 class="card-title">Nasi Goreng -  Skyline</h5>
+                                              <p class="card-text">Aneka Nasi Goreng, Aneka Minuman</p>
+                                              <div class="number">
+                                                <div class="medium infoitem"></div>
+                                                "4.6"                     1,2 km
+                                        </div>
+                                            </div>
+                                          </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://www.itrip.id/wp-content/uploads/2019/12/Tempat-Makan-Kediri.jpg" alt="...." />
+                                            <div class="card-body">
+                                              <h5 class="card-title">Ayam Bakar - Sabang</h5>
+                                              <p class="card-text">Aneka Ayam, Minuman, Camilan</p>
+                                              <div class="number">
+                                                <div class="medium infoitem"></div>
+                                                "4.6"                     1,2 km
+                                        </div>
+                                            </div>
+                                          </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top" src="https://www.itrip.id/wp-content/uploads/2019/12/Tempat-Makan-Kediri.jpg" alt="...." />
+                                            <div class="card-body">
+                                              <h5 class="card-title">Mie Ayam - H.Mamat</h5>
+                                              <p class="card-text">Mie Ayam Ceker, Bakso, Pangsit</p>
+                                              <div class="number">
+                                                <div class="medium infoitem"></div>
+                                                "4.6"                     1,2 km
+                                        </div>
+                                            </div>
+                                          </div>
+                                    </div>
+                                </div>
                                 {{-- @foreach ($wisatas as $wisata)
                                 <div class="col-sm-6 col-md-4 col-lg-3">
                                     <div class="border-0 shadow rounded card-travel text-center d-flex flex-column" style="background-image: url('{{ urlencode($wisata->gambar) }}');" >
